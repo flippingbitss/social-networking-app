184ab0b30288630550b9405d5ed0090dd2a02d72
Eddie Song <eddie.y.song@gmail.com>
2017-11-11 21:52:19 -0500

    Trims input values on createUser page

--- src/client/containers/CreateUser/index.js
+++ src/client/containers/CreateUser/index.js
@@ -9,6 +9,21 @@ import Button from 'material-ui/Button';
 import NavBar from "src/client/components/NavBar";
 import createUser from "./createUser.gql";
 
+const styles = theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit,
+      width: 200,
+    },
+    menu: {
+      width: 200,
+    },
+  });
+
 @graphql(createUser)
 class CreateUser extends React.Component {
 
@@ -22,36 +37,36 @@ class CreateUser extends React.Component {
     }
 
     render() {
-        const props = this.props;
+        const { classes } = this.props;
 
         return (
             <div>
                 <NavBar />
                 <div className="body-content">
-                    <form className={props.container} noValidate autoComplete="off">
+                    <form className={classes.container} noValidate autoComplete="off">
                         <TextField
                             id="firstName"
                             label="First Name"
-                            className={props.textField}
+                            className={classes.textField}
                             onChange={event => this.updateInputValue(event)}
                             margin="normal"
                         /><br />
                         <TextField
                             id="lastName"
                             label="Last Name"
-                            className={props.textField}
+                            className={classes.textField}
                             onChange={event => this.updateInputValue(event)}
                             margin="normal"
                         /><br />
                         <TextField
                             id="email"
                             label="Email Address"
-                            className={props.textField}
+                            className={classes.textField}
                             onChange={event => this.updateInputValue(event)}
                             margin="normal"
                         /><br />
 
-                        <Button raised color="primary" className={props.button} onClick={this.handleClick}>
+                        <Button raised color="primary" className={classes.button} onClick={this.handleClick}>
                             Submit
                         </Button>
 
@@ -63,7 +78,7 @@ class CreateUser extends React.Component {
 
     updateInputValue(event) {
         this.setState({
-            [event.target.id]: event.target.value != '' ? event.target.value : null
+            [event.target.id]: event.target.value.trim() != '' ? event.target.value.trim() : null
         });
     }
 
@@ -82,4 +97,4 @@ class CreateUser extends React.Component {
     }
 }
 
-export default CreateUser;
+export default withStyles(styles)(CreateUser);
