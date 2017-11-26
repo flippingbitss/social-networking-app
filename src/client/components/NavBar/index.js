68cc7616d914a6204c4cb9a4b5ec0300d17f803b
Myo Tint Aye <myotintaye00@gmail.com>
2017-11-26 12:38:45 -0500

    added drawer to navbar

--- src/client/components/NavBar/index.js
+++ src/client/components/NavBar/index.js
@@ -1,186 +1,60 @@
-import TextField from "material-ui/TextField";
-import Icon from "material-ui/Icon";
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "material-ui/styles";
-import Drawer from "material-ui/Drawer";
 import AppBar from "material-ui/AppBar";
 import Toolbar from "material-ui/Toolbar";
-import classNames from "classnames";
-import Typography from "material-ui/Typography";
-import Divider from "material-ui/Divider";
-import IconButton from "material-ui/IconButton";
+import Text from "material-ui/Typography";
 import Button from "material-ui/Button";
-import NavBarList from "src/client/components/NavBarList";
+import IconButton from "material-ui/IconButton";
+import TextField from "material-ui/TextField";
+import Icon from 'material-ui/Icon';
 
-const drawerWidth = 240;
 
 const styles = theme => ({
   root: {
-    width: "100%",
-    zIndex: 1,
-    overflow: "hidden"
+    width: "100%"
   },
   flex: {
     flex: 1
   },
-  icon: {
-    padding: theme.spacing.unit * 3
-  },
-  appFrame: {
-    position: "relative",
-    display: "flex",
-    width: "100%",
-    height: "100%"
-  },
-  appBar: {
-    position: "absolute",
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
   menuButton: {
-    marginLeft: 12,
+    marginLeft: -12,
     marginRight: 20
   },
-  hide: {
-    display: "none"
-  },
-  drawerPaper: {
-    position: "relative",
-    height: "100%",
-    width: drawerWidth
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: "0 8px",
-    ...theme.mixins.toolbar
-  },
-  content: {
-    width: "100%",
-    marginLeft: -drawerWidth,
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    height: "calc(100% - 56px)",
-    marginTop: 56,
-    [theme.breakpoints.up("sm")]: {
-      content: {
-        height: "calc(100% - 64px)",
-        marginTop: 64
-      }
-    }
-  },
-  contentShift: {
-    marginLeft: 0,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
+  icon: {
+    padding: theme.spacing.unit * 3
   }
 });
 
-class NavBar extends React.Component {
-  state = {
-    open: false
-  };
+const NavBar = props => {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            aria-label="Menu">
+            <Icon color="contrast">menu</Icon>
+          </IconButton>
+          <Text type="title" color="inherit" className={classes.flex}>
+            My App Bar
+          </Text>
+          <Icon color="contrast">search</Icon>
+          <TextField
+            autoFocus={true}
+            value={"my test search "}
+          />
+          <Button color="contrast">Logout</Button>
 
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleDrawerClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    const { classes, theme } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <div className={classes.appFrame}>
-          <AppBar
-            className={classNames(
-              classes.appBar,
-              this.state.open && classes.appBarShift
-            )}>
-            <Toolbar disableGutters={!this.state.open}>
-              <IconButton
-                color="contrast"
-                aria-label="open drawer"
-                onClick={this.handleDrawerOpen}
-                className={classNames(
-                  classes.menuButton,
-                  this.state.open && classes.hide
-                )}>
-                <Icon color="contrast">menu</Icon>
-              </IconButton>
-              <Typography
-                className={classes.flex}
-                type="title"
-                color="inherit"
-                noWrap>
-                My Awesome App
-              </Typography>
-
-              <Icon color="contrast">search</Icon>
-              <TextField autoFocus value={"my test search "} />
-              <Button color="contrast">Logout</Button>
-            </Toolbar>
-          </AppBar>
-          <Drawer
-            type="persistent"
-            classes={{
-              paper: classes.drawerPaper
-            }}
-            open={this.state.open}>
-            <div className={classes.drawerInner}>
-              <div className={classes.drawerHeader}>
-                <IconButton onClick={this.handleDrawerClose}>
-                  {theme.direction === "rtl" ? (
-                    <Icon>keyboard_arrow_right</Icon>
-                  ) : (
-                    <Icon>keyboard_arrow_left</Icon>
-                  )}
-                </IconButton>
-              </div>
-              <Divider />
-              <NavBarList className={classes.list} />
-              <Divider />
-            </div>
-          </Drawer>
-          <main
-            className={classNames(
-              classes.content,
-              this.state.open && classes.contentShift
-            )}>
-            {this.props.mainComponent}
-          </main>
-        </div>
-      </div>
-    );
-  }
-}
-
-NavBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
-  mainComponent: PropTypes.object.isRequired
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
-export default withStyles(styles, { withTheme: true })(NavBar);
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(NavBar);
